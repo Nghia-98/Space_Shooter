f07f129994d22865fee48f11dc45d8eeb44722ff
HoangNghia <hoangnghia.binhthuan@gmail.com>
2021-12-01 22:26:42 +0700

    config viewport

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    private Vector2 rawInput;
+
+    // Config view port
+    Vector2 minBounds;
+    Vector2 maxBounds;
+
+    [SerializeField] float paddingLeft;
+    [SerializeField] float paddingRight;
+    [SerializeField] float paddingTop;
+    [SerializeField] float paddingBottom;
+
+
+    void Start()
+    {
+        InitBounds();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Move();
+    }
+
+    private void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+    }
+
+    private void Move()
+    {
+        Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight);
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + paddingBottom, maxBounds.y - paddingTop);
+        transform.position = newPos;
+    }
+
+    private void OnMove(InputValue value)
+    {
+        rawInput = value.Get<Vector2>();
+        Debug.Log(rawInput);
+    }
+}
