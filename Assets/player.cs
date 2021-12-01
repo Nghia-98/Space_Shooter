d23fb84de88f837baaa1aa59ab8d005c4b589502
HoangNghia <hoangnghia.binhthuan@gmail.com>
2021-12-01 18:13:38 +0700

    add & config input system to player_game_object

---
+++ Assets/player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    Vector2  rawInput;
+    // Update is called once per frame
+    void Update()
+    {
+      Move();
+    }
+
+    void Move() {
+      Vector3 delta = rawInput * moveSpeed *Time.deltaTime;
+      transform.position += delta;
+    }
+    void OnMove(InputValue value) {
+      rawInput = value.Get<Vector2>();
+      Debug.Log(rawInput);
+    }
+}
