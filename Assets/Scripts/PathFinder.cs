34d8b7d1acf57b13a1075d167aa7014c8ea06872
HoangNghia <hoangnghia.binhthuan@gmail.com>
2021-12-02 20:03:27 +0700

    Instantiating Enemies

--- Assets/Scripts/PathFinder.cs
+++ Assets/Scripts/PathFinder.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PathFinder : MonoBehaviour {
-    [SerializeField] WaveConfigSO waveConfig;
+
+    EnemySpawner enemySpawner;
+    WaveConfigSO waveConfig;
+
     List<Transform> waypoints;
     int waypointIndex = 0;
 
+    void Awake() {
+        enemySpawner = FindObjectOfType<EnemySpawner>();
+    }
+
+
     void Start() {
+        waveConfig = enemySpawner.GetCurrentWave();
         waypoints = waveConfig.GetWaypoints();
         transform.position = waypoints[waypointIndex].position;
     }
@@ -23,7 +32,7 @@ public class PathFinder : MonoBehaviour {
             Vector2 targetPosition = waypoints[waypointIndex].position;
 
             transform.position = Vector2.MoveTowards(transform.position, targetPosition, delta);
-    
+
             if ((Vector2)transform.position == targetPosition) {
                 waypointIndex++;
             }
