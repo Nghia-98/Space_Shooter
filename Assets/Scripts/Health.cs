9267221e9678332fcba3bec4010d7866dc59eeb9
Nghia-98 <hoangnghia.binhthuan@gmail.com>
2022-01-09 14:29:45 +0700

    config explosion effect

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Health : MonoBehaviour {
-    [SerializeField] int health = 50;
+    [SerializeField] int health = 20;
 
     void OnTriggerEnter2D(Collider2D other) {
         DamageDealer damageDealer = other.GetComponent<DamageDealer>();
